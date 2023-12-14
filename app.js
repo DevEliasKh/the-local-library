40ced904a99eddbada08aa8d84ead2d1bde5a575
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2023-12-14 11:27:42 +0330

    make models and put data in mongoDB

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ var coolRouter = require("./routes/cool");
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.lzn1rsu.mongodb.net/?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+   await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
